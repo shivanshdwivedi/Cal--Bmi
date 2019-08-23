412666e9220eec90e643b7a07bc68f1a9e11c93f
Shivansh Dwivedi <shivdwi043@gmail.com>
2019-08-23 23:01:15 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/",function(req,res){
+
+res.sendFile(__dirname + "/index.html");
+
+});
+
+
+app.post("/",function(req,res){
+
+ var height = Number(req.body.num1);
+ var weight = Number(req.body.num2);
+ var result = weight/(height*height);
+
+ res.send("Your BMI is :" + result);
+
+});
+
+app.listen(3000,function(){
+
+console.log("your server is working on port 3000");
+
+});
